a1600eea6dd4dd2783636eee0e96482ec7c647b3
daryboutdboul <d_reboul_salze@hotmail.com>
2016-03-05 10:42:33 +0100

    tentative commite
    dary

---
+++ parissportifs/parissportifs/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\papa\documents\visual studio 2015\Projects\parissportifs\parissportifs\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
